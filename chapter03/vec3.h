82ed9ecf79c7d1305c5ef533371d10b73f694c0b
yu_kusanagi <kusanagi@on-s.co.jp>
2018-03-03 16:50:59 +0900

    extend vec3.h

--- chapter03/vec3.h
+++ chapter03/vec3.h
@@ -162,4 +162,19 @@ inline vec3 unit_vector(vec3 v)
   return v / v.length();
 }
 
+class ray
+{
+public:
+  ray() {}
+  ray(const vec3 &a, const vec3 &b)
+  {
+    A = a;
+    B = b;
+  }
+  vec3 origin() const { return A; }
+  vec3 direction() const { return B; }
+  vec3 point_at_parameter(float t) const { return A + t * B; }
+  vec3 A;
+  vec3 B;
+};
 #endif
